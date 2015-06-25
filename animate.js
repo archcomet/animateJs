b9f198189d5d569f1b931c62374febef76682d89
michaelg <archcomet@gmail.com>
2015-06-25 09:58:22 -0700

    Added prop parser

--- animate.js
+++ animate.js
@@ -1,7 +1,12 @@
 (function () {
 	'use strict';
 
+	function isString(obj) {
+		return  Object.prototype.toString.call(obj) === '[object String]';
+	}
+
 	var tasks = [],
+		tasksToRemove = [],
 		lastTimeStamp = null;
 
 	var scheduler = {
@@ -18,10 +23,20 @@
 		},
 
 		remove: function (task) {
-			var idx = tasks.indexOf(task);
-			if (idx !== -1) {
-				tasks.splice(idx, 1);
+			var idx = tasksToRemove.indexOf(task);
+			if (idx === -1) {
+				tasksToRemove.push(task);
 			}
+		},
+
+		drain: function() {
+			for (var i = 0, n = tasksToRemove.length; i < n; ++i) {
+				var idx = tasks.indexOf(tasksToRemove[i]);
+				if (idx !== -1) {
+					tasks.splice(idx, 1);
+				}
+			}
+			tasksToRemove.length = 0;
 		},
 
 		scheduleNext: function () {
@@ -39,18 +54,23 @@
 				timeStamp = performance.now(),
 				dt = timeStamp - lastTimeStamp;
 
+			scheduler.drain();
+
 			for (; i < n; ++i) {
 				tasks[i].step(dt);
 			}
 
-			lastTimeStamp = timeStamp;
+			scheduler.drain();
 			scheduler.scheduleNext();
+
+			lastTimeStamp = timeStamp;
 		}
 	};
 
 	function Task() {
 		var doneFilters = [],
 			progressFilters = [],
+			started = false,
 			task = this,
 			elapsed = 0,
 			delay = 0,
@@ -64,6 +84,7 @@
 					scheduler.add(task);
 					elapsed = 0;
 					delay = delaySetting;
+					started = false;
 
 					// todo state
 
@@ -72,6 +93,11 @@
 
 				stop: function () {
 					scheduler.remove(task);
+					return promise;
+				},
+
+				resume: function() {
+					// todo
 					return promise;
 				},
 
@@ -101,40 +127,134 @@
 				}
 			};
 
+		task.notify = function() {
+			for (var i = 0, n = progressFilters.length; i < n; ++i) {
+				progressFilters[i].apply(promise, arguments);
+			}
+ 		};
+
+		task.resolve = function() {
+			for (var i = 0, n = doneFilters.length; i < n; ++i) {
+				doneFilters[i].apply(promise, arguments);
+			}
+		};
+
 		task.promise = promise;
 
 		task.step = function(dt) {
-
 			if (delay > 0) {
 				delay -= dt;
 				dt = delay >= 0 ? 0 : Math.abs(delay);
 			}
 
 			if (dt > 0) {
+				if (!started) {
+					started = true;
+					task.begin();
+				}
 				elapsed += dt;
-				console.log(elapsed);
+				task.update(elapsed);
 			}
-
 		};
 	}
 
-	var animate = function (obj) {
 
-		var t = new Task(),
-			promise = t.promise;
+	function merge(target, source, destinationKey) {
+		if (source) {
+			for (var key in source) {
+				if (source.hasOwnProperty(key)) {
+					target[key] = target[key] || {};
+					target[key][destinationKey] = source[key];
+				}
+			}
+		}
+	}
 
-		promise.from = function () {
-			// todo
+	var floatRegExp = /^([-+]?[0-9]*\.?[0-9]+([eE][-+]?[0-9]+)?)(.*)/;
+
+	function format(target, source) {
+		var value, valueString, matches;
+		for (var key in target) {
+			if (target.hasOwnProperty(key)) {
+				if (target[key].from === undefined) {
+					target[key].from = source[key];
+				}
+
+				if (target[key].to === undefined) {
+					target[key].to = source[key];
+				}
+
+				if (isString(target[key].from)) {
+					valueString = target[key].from;
+					value = parseFloat(valueString);
+
+					if (!isNaN(value)) {
+						matches = valueString.match(floatRegExp);
+						target[key].suffix = matches[3];
+						target[key].from = value;
+					}
+				}
+
+				if (isString(target[key].to)) {
+					valueString = target[key].to;
+					value = parseFloat(valueString);
+
+					if (!isNaN(value)) {
+						matches = valueString.match(floatRegExp);
+						target[key].suffix = matches[3];
+						target[key].to = value;
+					}
+				}
+			}
+		}
+	}
+
+	var animate = function (target) {
+
+		var task = new Task(),
+			easingFn = animate.easing.LINEAR,
+			durationSetting = 0,
+			fromSetting = null,
+			toSetting = null,
+			props,
+			promise = task.promise;
+
+		task.begin = function() {
+			props = {};
+			merge(props, fromSetting, 'from');
+			merge(props, toSetting, 'to');
+			format(props, target);
+		};
+
+		task.update = function(elapsed) {
+			//todo
+			console.log('update', elapsed);
+		};
+
+		promise.from = function (descriptor, duration, doneFilter) {
+			fromSetting = descriptor;
+			durationSetting = duration;
+			if (doneFilter) {
+				promise.done(doneFilter);
+			}
 			return promise;
 		};
 
-		promise.to = function () {
-			// todo
+		promise.to = function (descriptor, duration, doneFilter) {
+			toSetting = descriptor;
+			durationSetting = duration;
+			if (doneFilter) {
+				promise.done(doneFilter);
+			}
 			return promise;
 		};
 
-		promise.using = function () {
-			// todo
+		promise.duration = function(duration) {
+			durationSetting = duration;
+		};
+
+		promise.using = function (easing) {
+			easingFn = easing;
 			return promise;
 		};
 
@@ -166,9 +286,11 @@
 		smoothStartN: function (n) {
 			// todo
 		},
+
 		smoothStopN: function (n) {
 			// todo
 		},
+
 		smoothStepN: function (n) {
 			// todo
 		},
